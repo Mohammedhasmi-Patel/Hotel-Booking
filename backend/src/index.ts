eed60510bf47fd975535e7dbd603ab2bc6fdb29c
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-03-06 00:09:11 +0530

    setup frontend and backend environment

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
